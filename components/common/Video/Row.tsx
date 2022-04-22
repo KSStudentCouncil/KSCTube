14e166ab9825ac39fd4ff11f17ea0be0323d3be3
p1atdev <shu_teiei@outlook.jp>
2022-04-22 18:25:02 +0900

    feat: bookmark button ui

--- components/common/Video/Row.tsx
+++ components/common/Video/Row.tsx
@@ -2,12 +2,12 @@ import { Icon } from '@iconify/react'
 import Link from 'next/link'
 import { ComponentProps, useState } from 'react'
 import { Video } from '../../../types/video'
+import BookmarkButton from '../../ui/Video/BookmarkButton'
 
 type Props = Video & ComponentProps<'div'>
 
 const VideoRow = ({ ...props }: Props) => {
   const [isBooked, setIsBooked] = useState(false)
-
   return (
     <div {...props} className={`w-xl ${props.className}`}>
       <Link href={`/wathc/${props.id}`}>
@@ -21,23 +21,13 @@ const VideoRow = ({ ...props }: Props) => {
           <p className="opacity-75">{props.creator}</p>
         </div>
         <div className="mr-2 mt-2 text-xl">
-          <button
-            className={`z-20 h-7 w-7 rounded-md hover:bg-slate-600  ${
-              isBooked ? 'text-slate-500' : 'text-blue-500 '
-            }`}
+          <BookmarkButton
+            isBooked={isBooked}
             onClick={() => {
               // TODO: ここらへんのまじめな実装
               setIsBooked(!isBooked)
             }}
-          >
-            <div className="m-auto h-5 w-5">
-              {/* // TODO: ブックマーク状態で切り替え */}
-              <Icon
-                icon={isBooked ? 'bi:bookmark' : 'bi:bookmark-fill'}
-                className={`h-full w-full`}
-              />
-            </div>
-          </button>
+          />
         </div>
       </div>
     </div>
---
+++ components/ui/Video/BookmarkButton.tsx
@@ -0,0 +1,55 @@
+import { Icon } from '@iconify/react'
+import { ComponentProps, useEffect, useState } from 'react'
+import { motion, useAnimation } from 'framer-motion'
+
+type Props = {
+  isBooked: boolean
+} & ComponentProps<'button'>
+
+const BookmarkButton = ({ isBooked, ...props }: Props) => {
+  const controlls = useAnimation()
+
+  const bookedAnimation = {
+    scale: [1, 1.2, 1, 1],
+    transition: {
+      duration: 0.4,
+      ease: 'easeInOut',
+    },
+  }
+
+  const unbookedAnimation = {
+    y: [0, 5, 0],
+    transition: {
+      duration: 0.3,
+      ease: 'easeInOut',
+    },
+  }
+
+  useEffect(() => {
+    if (isBooked) {
+      controlls.start(bookedAnimation)
+    } else {
+      controlls.start(unbookedAnimation)
+    }
+  }, [isBooked])
+
+  return (
+    <button
+      {...props}
+      className={`z-20 h-7 w-7 rounded-md hover:bg-slate-600 hover:bg-opacity-75  ${
+        isBooked ? 'text-blue-500' : 'text-slate-500 '
+      } ${props.className}`}
+    >
+      <motion.div animate={controlls} className="m-auto h-5 w-5">
+        {/* // TODO: ブックマーク状態で切り替え */}
+
+        <Icon
+          icon={isBooked ? 'bi:bookmark-fill' : 'bi:bookmark'}
+          className={`h-full w-full`}
+        />
+      </motion.div>
+    </button>
+  )
+}
+
+export default BookmarkButton
