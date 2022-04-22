38b0b4704212817a0d2b8936ea7b5f1123c43bcb
p1atdev <shu_teiei@outlook.jp>
2022-04-23 00:04:02 +0900

    wip: index page ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,12 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
 import Link from 'next/link'
-import { useContext } from 'react'
-import Avatar from '../components/common/Avatar'
-import { AuthContext } from '../components/context/auth'
 
 const Home: NextPage = () => {
-  const { user } = useContext(AuthContext)
-
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2 ">
-      <div>
-        <div>
+    <div className="flex min-h-screen flex-col  justify-center py-2 ">
+      <div className="flex">
+        {/* <div>
           {user ? (
             <div className="flex items-center p-2">
               <Avatar name={user.uid} className={'pr-2'} />
@@ -19,12 +15,38 @@ const Home: NextPage = () => {
           ) : (
             <></>
           )}
+        </div>*/}
+        <div className="ml-20 w-full">
+          <div className="font-black">
+            <p className="text-4xl">海城</p>
+            <h3 className="text-5xl">部活紹介動画</h3>
+          </div>
+          <p className=" my-6 max-w-sm text-sm">
+            海城に存在する、さまざまな部活の新入生向け部活紹介動画の閲覧用サイトです。
+            利用するには、学校から配られた Microsoft アカウント
+            (12340123@stu.kaijo.ed.jp のような) が必要になります。
+            自分のアカウントやパスワードがわからない場合は、担任の先生などに直接聞いてください。
+          </p>
+
+          <Link href={'/login'}>
+            <button className="rounded-sm bg-blue-500 py-3 px-5 font-bold text-white shadow-md transition-all duration-200 ease-in-out hover:shadow-blue-500/20">
+              ログイン
+            </button>
+          </Link>
         </div>
-        <Link href={'/login'}>
-          <button className="rounded-sm bg-blue-500 py-3 px-5 font-bold text-white">
-            Sign in
-          </button>
-        </Link>
+        <div className="my-auto mr-20 rotate-6 -skew-y-3 skew-x-3  transition-transform duration-500 ease-in-out hover:rotate-0 hover:skew-x-0 hover:skew-y-0">
+          {/* 画像 */}
+          <div className="my-auto items-center justify-center ">
+            <Image
+              src={
+                'https://unsplash.com/photos/iKdQCIiSMlQ/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjUwNjM3MjQ3&force=true'
+              }
+              width="640px"
+              height="420px"
+              className="m-auto rounded-lg object-cover shadow-2xl shadow-gray-500/50 saturate-50"
+            />
+          </div>
+        </div>
       </div>
     </div>
   )
